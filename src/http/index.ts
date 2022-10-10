d21c2b48f98cfc998c18c32eb88a1fce87551109
Felipe <lipe.h.r@gmail.com>
2022-10-10 10:08:47 -0300

    pasta http

---
+++ src/http/index.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError } from "axios";
+import { history } from "../App";
+
+const http = axios.create({
+  baseURL: 'http://localhost:8000',
+  headers: {
+    Accept: 'application/json',
+    Content: 'application/json'
+  }
+})
+
+http.interceptors.request.use(function (config) {
+  const token = sessionStorage.getItem('token')
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config;
+}, function (error) {
+  console.log('%cindex.ts line:19 Erro', 'color: #007acc;', 'Erro no interceptor do axios');
+  return Promise.reject(error)
+});
+
+http.interceptors.response.use(function (response) {
+  return response;
+}, function (error: AxiosError) {
+  if (error.response?.status === 401) {
+    history.push('/')
+    return Promise.reject()
+  }
+  return Promise.reject(error);
+});
+
+export default http
