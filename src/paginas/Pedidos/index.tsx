e35c5bb5e6a4655f539e28fe68cd713d773046d8
Felipe <lipe.h.r@gmail.com>
2022-10-10 10:08:13 -0300

    encapsulando axios

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -1,8 +1,8 @@
 import { AbBotao } from "react-alurabooks"
-import axios from "axios"
-import './Pedidos.css'
 import { useEffect, useState } from "react"
 import { IPedido } from "../../interfaces/IPedido"
+import http from "../../http"
+import './Pedidos.css'
 
 const Pedidos = () => {
   const formatador = Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' })
@@ -10,24 +10,16 @@ const Pedidos = () => {
   const [pedidos, setPedidos] = useState<IPedido[]>([])
 
   useEffect(() => {
-    const token = sessionStorage.getItem('token')
-    axios.get<IPedido[]>('http://localhost:8000/pedidos', {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    }).then(resposta => setPedidos(resposta.data))
+    http.get<IPedido[]>('pedidos')
+      .then(resposta => setPedidos(resposta.data))
       .catch(erro => console.log('%cindex.tsx line:16 erro', 'color: #ff0000;', erro))
   }, [])
 
   const excluir = (pedido: IPedido) => {
-    const token = sessionStorage.getItem('token')
-    axios.delete('http://localhost:8000/pedidos/' + pedido.id, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    }).then(() => {
-      setPedidos(pedidos.filter(p => p.id !== pedido.id))
-    })
+    http.delete('pedidos' + pedido.id)
+      .then(() => {
+        setPedidos(pedidos.filter(p => p.id !== pedido.id))
+      })
       .catch(erro => console.log('%cindex.tsx line:16 erro', 'color: #ff0000;', erro))
   }
 
@@ -42,7 +34,7 @@ const Pedidos = () => {
             <li>Valor total: <strong>{formatador.format(pedido.total)}</strong></li>
             <li>Entrega realizada em: <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
             <li>
-              <button onClick={() => excluir(pedido)}>Excluir</button>
+              <button className="btn" onClick={() => excluir(pedido)}>Excluir</button>
             </li>
           </ul>
           <AbBotao texto="Detalhes" />
